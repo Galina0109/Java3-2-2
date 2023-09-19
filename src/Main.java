39211d547376132a100fbc968c6922dd4a4f2887
Galina <gana88tit@gmail.com>
2023-09-19 18:22:00 +0300

    change Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 100; // баланс на счету
-        int y = 1300; //пополнение счета
+        int balance = 1400; // баланс на счету
+        int popolnenie = 1520; //пополнение счета
 
         int bonus;
-        if ((x + y) > 1000) {
-            System.out.println("Количество бонусов за баланс более 1000 рублей:");
-            System.out.println(bonus = y / 100);
+        if ((balance + popolnenie) > 1000) {
+        System.out.println("Количество бонусов за баланс более 1000 рублей:");
+        System.out.println(bonus = popolnenie / 100);
         }
 
         System.out.println("Итоговая сумма на счету клиента:");
-        System.out.println(x + y + (y / 100));
+        System.out.println(balance + popolnenie + (popolnenie / 100));
     }
 }
